af05f9e99e13cfa631a7b9dbf51d0a74dbf2b093
Dfria <jdfria@gmail.com>
2023-03-06 09:38:40 -0800

    Added Titanic data

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+TRAIN_PATH = 'train.csv'
+TEST_PATH = 'test.csv'
+
+train_data = pd.read_csv(TRAIN_PATH)
+test_data = pd.read_csv(TEST_PATH)
+
+print(train_data.columns())
